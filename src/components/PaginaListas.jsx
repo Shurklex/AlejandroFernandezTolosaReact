f05b12391d43a8747979c30272306575c5e250fe
Shurklex <alejandro.fernandez.tolosa@gmail.com>
2022-01-11 08:20:21 +0100

    Cambios ultima clase

---
+++ src/components/PaginaListas.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Lista } from './Lista';
+import ListaClase from './ListaClase';
+export function PaginaListas() {
+  const elementos1 = [
+    { done: false, texto: 'Limpiar el Cajon de Arena', prioridad: 'alta' },
+    { done: true, texto: 'Hacer la Lista de la Compra', prioridad: 'media' },
+  ];
+  const elementos2 = [
+    { done: false, texto: 'Papel' },
+    { done: true, texto: 'Queso' },
+    { done: false, texto: 'Chocolate' },
+  ];
+  const elementos3 = [{ done: false, texto: 'Tarea SGE' }];
+
+  return (
+    <div>
+      <h1>Listas de tareas</h1>
+      <ListaClase titulo="Tareas de la casa" icono="♥" elementos={elementos1} />
+      <ListaClase
+        titulo="Lista de la compra"
+        icono="✌"
+        elementos={elementos2}
+      />
+      <ListaClase titulo="ToDos del curso" icono="🙌" elementos={elementos3} />
+    </div>
+  );
+}
